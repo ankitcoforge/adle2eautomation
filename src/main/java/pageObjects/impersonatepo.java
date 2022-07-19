7c1eb2d8e6b23074958fec570bbb015e869c4fdd
Ankit Srivastava <ankit.srivastava@aulcorp.local>
2022-07-19 02:51:41 -0700

    Impersonate Test Cases completed

--- src/main/java/pageObjects/impersonatepo.java
+++ src/main/java/pageObjects/impersonatepo.java
@@ -2,11 +2,16 @@ package pageObjects;
 
 import utils.baseClass;
 
-public class impersonateMenu extends baseClass{
+public class impersonatepo extends baseClass{
 	
+	public String role1 = "ctl00_mainContent_TextBoxRoleIdentifier";
 	public String roleDropdown = "ctl00_mainContent_DropDownListRoleType";
     public String roleId = "#cnt table #container table tbody tr input";
     public String button = "ctl00_mainContent_ButtonGetUsers";
     public String tableFirstRow = "ctl00_mainContent_ASPxGridViewUsers_cell0_11_ASPxButtonImpersonate";
+    public String logoutArrow  = "adl-header > header > div > div > button > mat-icon";
+    public String impersonate = "div [role='menu'] > div > ul >li >button";
     
+	
+	
 }
