13fdbad7b16b8a23a606e0a34e477091970264b6
Divyasree Telaprolu - Coforge <divyasree.telaprolu@aulcorp.local>
2025-01-21 16:43:10 +0530

    gap file added

--- src/test/java/testsuite/ManageVSC_GAPprefrences_test.java
+++ src/test/java/testsuite/ManageVSC_GAPprefrences_test.java
@@ -1,27 +1,14 @@
 package testsuite;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
-import javax.swing.text.Utilities;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
